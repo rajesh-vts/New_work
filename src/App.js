7a1164b1090ac5c470142f85f5a2f72f7c593694
noob-raj <139133766+noob-raj@users.noreply.github.com>
2023-12-14 09:58:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Routes, Route, Router} from 'react-router-dom';
 import Sidebar from './components/Layout/Sidebar';
 import AdminRoutes from './routes/admin';
 // import CustomerRoutes from './routes/customer';
-import OrderList from './components/Orders/list.js';
-import ProductList from './components/Products/list.js';
 
 import './App.css'; 
 
@@ -16,12 +13,7 @@ function App() {
         <Sidebar />
         <div className="main-content">
           <Routes>
-            {/* Define your admin routes */}
-            {/* <AdminRoutes path="/dashboard" exact>
-            <OrderList />
-            </AdminRoutes> */}
             <Route path="/admin/*" element={<AdminRoutes />} />
-            {/* You can add more routes here */}
           </Routes>
         </div>
       </div>
